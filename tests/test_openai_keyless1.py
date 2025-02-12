7a2f2232ddfdee3a0384843739a841a8e20918a3
Chandan Singh <chandan_singh@berkeley.edu>
2025-02-12 09:26:04 -0800

    add azurecli credential

--- tests/test_openai_keyless1.py
+++ tests/test_openai_keyless1.py
@@ -1,22 +1,24 @@
-import os
-import openai
 from openai import AzureOpenAI
-from azure.identity import DefaultAzureCredential, get_bearer_token_provider
+from azure.identity import DefaultAzureCredential, get_bearer_token_provider, AzureCliCredential
 
-azure_credential = DefaultAzureCredential()
-token_provider = get_bearer_token_provider(azure_credential,
-                                           "https://cognitiveservices.azure.com/.default")
+# azure_credential = DefaultAzureCredential()
+azure_credential = AzureCliCredential()
+token_provider = get_bearer_token_provider(
+    azure_credential,
+    "https://cognitiveservices.azure.com/.default"
+)
 
 
 if __name__ == '__main__':
     client = AzureOpenAI(
-        api_version="2024-02-15-preview",
+        api_version="2024-03-01-preview",
         azure_endpoint="https://dl-openai-1.openai.azure.com/",
         azure_ad_token_provider=token_provider,
     )
 
     response = client.chat.completions.create(  # replace this value with the deployment name you chose when you deployed the associated model.
-        model='gpt-4o',
+        # model='gpt-4o',
+        model='gpt-4o-mini',
         messages=[
             {"role": "system", "content": "You are a helpful assistant."},
             {"role": "user", "content": "2+3="}
